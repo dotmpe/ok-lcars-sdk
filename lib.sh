46332a7a04e5206c61d01b664535d91193ceb669
B. van Berkum <dev@dotmpe.com>
2019-12-06 04:49:45 +0100

    run-dev

--- lib.sh
+++ lib.sh
@@ -6,12 +6,11 @@ sf_sh_volumes()
   volumes=
   test ! -e /etc/localtime ||
     volumes=" --volume $(realpath /etc/localtime):/etc/localtime:ro"
-  test ! -e /srv/project-local/node-sitefile/.git ||
-    volumes=" --volume /srv/project-local/node-sitefile/tools/docker/ubuntu/entry.sh:/usr/local/share/sitefile/entry.sh"
 }
 
 wait_for_container()
 {
+  test -n "$1" || return
   while true
   do
     docker ps | grep " \<$1\>" || { sleep .2; continue; }
--- run-dev.sh
+++ run-dev.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -e
 
-# dev mode (ro); mount local source, also use latest sitefile entry.sh iso. built-in
-
-#script_dir=/opt/node-sitefile
-#  --volume $script_dir/tools/docker/ubuntu/entry.sh:/usr/local/share/sitefile/entry.sh:ro \
+# dev mode (ro); mount local source, XXX: also use latest sitefile entry.sh iso. built-in
 
 sf_version=0.0.7-dev
-sf_version=0.0.7-test
+#sf_version=0.0.7-test
+#sf_version=latest
 
 # r/o so don't touch git or npm
 src_update=0
@@ -19,19 +17,25 @@ site_ver=r0.0
 gh_keyfile=~/.ssh/id_rsa
 kbn=id_rsa
 
-hostname=$(hostname)
+hostname=$(hostname -f)
 
 mkdir -p srv
 ssh-keyscan github.com > srv/known_hosts
 
 . ./lib.sh
 
+volumes=
 sf_sh_volumes
 #volumes="$volumes --volume $(pwd -P)/srv/src:/src"
 #volumes="$volumes --volume $(pwd -P)/srv/home:/home/treebox"
+sf_dir=/srv/project-local/node-sitefile
+test ! -e $sf_dir/.git ||
+  volumes="$volumes --volume $sf_dir/tools/docker/ubuntu/entry.sh:/usr/local/share/sitefile/entry.sh:ro"
+
 set -x
 docker run \
   -d --name sf-ok-lcars-sdk-dev \
+  --domainname $hostname \
   -h $hostname -e SITEFILE_HOST=$hostname \
   -p 7011:7011 -e SITEFILE_PORT=7011 \
   -e src_update=$src_update \
@@ -39,8 +43,8 @@ docker run \
   --volume $(realpath $gh_keyfile):/home/treebox/.ssh/$kbn \
   --volume $(realpath ./srv/known_hosts):/home/treebox/.ssh/known_hosts \
   --volume $(realpath $(pwd)):/src/$site_src:ro \
-  dotmpe/node-sitefile:$sf_version \
+  bvberkum/node-sitefile:$sf_version \
   \
   "$site_src"
 
-# "$site_repo" "$site_ver"
+#
