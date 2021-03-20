1545060aced028e281263de1b8055ef80dacb032
B. van Berkum <dev@dotmpe.com>
2021-03-20 21:21:05 +0100

    More Shellcheck fixes

--- mirror.sh
+++ mirror.sh
@@ -2,16 +2,16 @@
 hostname=$(hostname)
 
 # dev
-: ${port:=7011}
+: "${port:=7011}"
 
 # ht pm2 'sf-ok-lcars-sdk'
 #port=4507
 
 set -xe
 
-httrack http://$hostname:$port -O mirror "+*.$hostname/*"
+httrack "http://$hostname:$port" -O mirror "+*.$hostname/*"
 
 rsync -avzui --delete \
-  ./mirror/${hostname}_$port/ ../dotmpe.github.io/ok-lcars-sdk
+  ./mirror/"${hostname}_$port"/ ../dotmpe.github.io/ok-lcars-sdk
 
 #
